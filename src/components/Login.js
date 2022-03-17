64bd79ec753e711bf5d8385cf22dc0ba8008e26a
chando <dave@staticum.net>
2022-03-17 15:44:45 +0800

    update minor clean-up of code

--- src/components/Login.js
+++ src/components/Login.js
@@ -56,3 +56,52 @@ export default function Login() {
     </>
   );
 }
+
+// If i had to manually hash and salt the pwd with bcrypt - here is what it would look like & also protect against SQL injection attacks
+const bcrypt = require("bcrypt");
+const db = require("psql");
+//set salting rounds
+const saltRounds = 10;
+var username = "buckRogers";
+var password = "I_l1k3candy";
+var values = [username]; // query values
+
+// query statement to store hash
+var storeSQL = "UPDATE user_table SET password = $1 WHERE username = $2";
+let lookupSQL = "select password from user_table where username = $1";
+
+//salt and encrypt and post
+bcrypt.hash(password, saltRounds, function (err, hash) {
+  let values = [hash, username]; // query values
+  // store hash in database
+  db.query(storeSQL, values, function (err, res) {
+    if (err) throw err;
+    else {
+      console.log("stored!");
+    }
+  });
+});
+
+// query database for user's password and compare
+db.query(statement, values, function (err, res) {
+  if (err) throw err;
+  else {
+    var hash = res.rows[0].password;
+    // compare hash and password
+    bcrypt.compare(password, hash, function (err, result) {
+      // execute code to test for access and login
+      hasAccess(result);
+    });
+  }
+});
+
+// function to log in
+function hasAccess(result) {
+  if (result) {
+    // insert login code here
+    console.log("Access Granted!");
+  } else {
+    // insert access denied code here
+    console.log("Access Denied!");
+  }
+}
